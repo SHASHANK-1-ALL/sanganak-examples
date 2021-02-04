4858f703045f331a7dee68064cdd28c051a8bb7e
Saurabh Joshi <sbjoshi@iith.ac.in>
2021-02-04 15:58:49 +0530

    fileio examples

---
+++ id1063-2020-21-cprograms/fileio_licensed.c
@@ -0,0 +1,58 @@
+/*    Copyright (C) 2021  Saurabh Joshi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *f;
+	f=fopen("newfile.txt","r");
+#if 0
+	int a=5;
+	int b=10;
+	if(f==NULL)
+	{
+		printf("Could not open file\n");
+	}	
+	else
+	{
+		int o1=fprintf(f,"File opening was successful\n");
+		printf("%p\n",f);
+		scanf("%d",&a);
+		int o2=fprintf(f,"a : %d, b: %d, a+b : %d\n",a,b,a+b);
+		printf("o1: %d, o2 : %d\n",o1,o2);
+		printf("%p\n",f);
+		fclose(f);
+	}
+#endif
+
+	if(f==NULL) return 1;
+	
+
+	int a,b,c,d,e;
+	for(unsigned i=1;;i++)
+	{
+	 int o1=fscanf(f, "%d",&b);
+	 printf("iteration : %d, value : %d\n",i,b);
+	scanf("%d",&a);
+	 if(o1 < 1 || f==NULL) break;
+	}
+	fclose(f);
+
+
+	return EXIT_SUCCESS;
+}
