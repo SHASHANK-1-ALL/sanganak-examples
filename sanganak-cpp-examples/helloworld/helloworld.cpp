b765c22b6940bdc29a8528d3332b2b5208bd3602
Saurabh Joshi <sbjoshi@iith.ac.in>
2021-04-08 21:12:55 +0530

    hello world example

---
+++ sanganak-cpp-examples/helloworld/helloworld.cpp
@@ -0,0 +1,37 @@
+/*    Copyright (C) 2021  Saurabh Joshi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <cstdio>
+
+namespace sanganak // namespace
+{
+ void printf(int a, int b)
+ {
+    std::printf("%d : %d",a,b);
+ }
+
+};
+
+
+int main()
+{
+    float f=5.5;
+    unsigned u=50;
+    std::cout << "Hello World!" << f << ":" << u << std::endl;
+    return 0;
+    
+}
